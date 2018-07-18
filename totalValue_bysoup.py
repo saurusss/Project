5d81161d57a955055f5c5b08ec9177b56f7aa2fe
saurusss <40891607+saurusss@users.noreply.github.com>
2018-07-19 01:05:45 +0900

    .

--- totalValue_bysoup.py
+++ totalValue_bysoup.py
@@ -4,7 +4,8 @@ from bs4 import BeautifulSoup
 url = "http://finance.naver.com/item/main.nhn?code=035420"
 html = requests.get(url).text
  
-soup = BeautifulSoup(html, "html5lib")
+# soup = BeautifulSoup(html, "html5lib")
+soup = BeautifulSoup(html, "html.parser")
 tags = soup.select("#_market_sum")
 cap = tags[0].parent.text
 cap = cap.replace('\t', '')
