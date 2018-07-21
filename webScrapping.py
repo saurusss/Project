574355e9fbef8cf4f8badd6ffc466722b76f0446
saurusss <40891607+saurusss@users.noreply.github.com>
2018-07-21 16:56:04 +0900

    180724@hhome

---
+++ webScrapping.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+ 
+url = "http://finance.naver.com/item/main.nhn?code=035420"
+html = requests.get(url).text
+ 
+soup = BeautifulSoup(html, "html5lib")
+tags = soup.select("#_market_sum")
+cap = tags[0].parent.text
+cap = cap.replace('\t', '')
+cap = cap.replace('\n', '')
+cap = cap.strip()
+print(cap)
