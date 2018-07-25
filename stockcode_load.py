36eb85d4035e1c2af5398f43d954d896b93c0a47
saurusss <40891607+saurusss@users.noreply.github.com>
2018-07-25 23:21:08 +0900

    .

--- stockcode_load.py
+++ stockcode_load.py
@@ -1,3 +1,9 @@
 import pandas as pd
-stockcode = pd.read_csv('c:\\TEMP\\stockcode.csv', index_col=0)
-print(stockcode)
+import pickle
+# stockcode = pd.read_csv('c:\\TEMP\\stockcode.csv', index_col=0)
+# print(stockcode)
+stock_df_file ='stockcodefile.data'
+f = open(stock_df_file, 'rb')
+# Load the object from the file
+storedlist = pickle.load(f)
+print(storedlist)
