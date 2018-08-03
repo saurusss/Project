2d0fbf9006715c5bbadc8f8f4ea629177c8a47ec
unknown <saurusss62@gmail.com>
2018-08-03 17:53:40 +0900

    180803@offce

--- study_PYQT.py
+++ study_PYQT.py
@@ -88,9 +88,29 @@
 #     app.exec_()
 
 #========================================
+# import sys
+# from PyQt5.QtWidgets import *
+# from PyQt5.QtCore import *
+
+# class MyWindow(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
+#         self.setupUI()
+
+#     def setupUI(self):
+#         btn1 = QPushButton("닫기", self)
+#         btn1.move(20, 20)
+#         btn1.clicked.connect(QCoreApplication.instance().quit)
+
+# if __name__ == "__main__":
+#     app = QApplication(sys.argv)
+#     mywindow = MyWindow()
+#     mywindow.show()
+#     app.exec_()
+
+#========================================
 import sys
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
 
 class MyWindow(QMainWindow):
     def __init__(self):
@@ -98,14 +118,31 @@ class MyWindow(QMainWindow):
         self.setupUI()
 
     def setupUI(self):
-        btn1 = QPushButton("닫기", self)
-        btn1.move(20, 20)
-        btn1.clicked.connect(QCoreApplication.instance().quit)
+        self.setGeometry(800, 400, 400, 150)
+
+        textLabel = QLabel("Message: ", self)
+        textLabel.move(20, 20)
+
+        self.label = QLabel("", self)
+        self.label.move(80, 20)
+        self.label.resize(150, 30)
+
+        btn1 = QPushButton("Click", self)
+        btn1.move(20, 60)
+        btn1.clicked.connect(self.btn1_clicked)
+
+        btn2 = QPushButton("Clear", self)
+        btn2.move(140, 60)
+        btn2.clicked.connect(self.btn2_clicked)
+
+    def btn1_clicked(self):
+        self.label.setText("버튼이 클릭되었습니다.")
+
+    def btn2_clicked(self):
+        self.label.clear()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     mywindow = MyWindow()
     mywindow.show()
     app.exec_()
-
-
